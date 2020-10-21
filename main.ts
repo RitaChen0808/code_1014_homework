3b9f3ff09ee60963ea5d048a5100a1993701e772
RitaChen0808 <71751332+RitaChen0808@users.noreply.github.com>
2020-10-21 15:51:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ basic.forever(function () {
         for (let X = 0; X <= 4; X++) {
             if (Y % 2 == 0) {
                 led.plot(4 - X, 4 - Y)
-            } else if (Y == 1) {
+            } else if (Y % 2 == 1) {
                 led.plot(X, 4 - Y)
             }
             basic.pause(100)
